7f52e7c24de1ab292ef917cb262fc967a6ec3fd8
Isaac James <chizuruokej@yahoo.com>
2022-02-04 13:17:38 +0100

    new update

--- src/components/payment-info/CardForm.jsx
+++ src/components/payment-info/CardForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Button, styled, TextField, Typography } from "@mui/material";
 import { space } from "../../assets/utils";
 
